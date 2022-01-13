a667d99b9d490b0dbd29c3cd86ff24baa33a9cce
YeetMaster467 <elicomixinc@gmail.com>
2022-01-12 19:54:55 -0800

    Completed Obedient Robot
    Happy Pet in progress

--- src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
+++ src/_03_method_writing/_1_obedient_robot/ObedientRobot.java
@@ -1,21 +1,94 @@
 package _03_method_writing._1_obedient_robot;
 
 import org.jointheleague.graphical.robot.Robot;
+
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 
 public class ObedientRobot {
 
 	static Robot rob = new Robot("mini");
 	static String color = JOptionPane.showInputDialog("What color do you want? Red, green, or blue.");
+	static String shape = JOptionPane.showInputDialog("What shape do you want? Square, circle, or triangle.");
 	
 	public static void main(String[] args) {
 		
-		rob.show();
+		if(shape.equalsIgnoreCase("square")) {
+			drawSquare();
+		}else if(shape.equalsIgnoreCase("circle")) {
+			drawCircle();
+		}else if(shape.equalsIgnoreCase("triangle")) {
+			drawTriangle();
+		}else {
+			JOptionPane.showMessageDialog(null, "That is not an avalible shape.");
+		}
+		
+		rob.hide();
 		
 	}
 	
 	static void drawSquare() {
 		
+		if(color.equalsIgnoreCase("red")) {
+			rob.setPenColor(Color.red);
+		}else if(color.equalsIgnoreCase("green")) {
+			rob.setPenColor(Color.green);
+		}else if(color.equalsIgnoreCase("blue")) {
+			rob.setPenColor(Color.blue);
+		}else {
+			rob.setRandomPenColor();
+		}
+		
+		rob.penDown();
+		rob.setSpeed(10);
+		for(int i=0; i<4; i++) {
+			rob.turn(90);
+			rob.move(100);
+		}
+		
 	}
+	
+	static void drawTriangle() {
 
+		if(color.equalsIgnoreCase("red")) {
+			rob.setPenColor(Color.red);
+		}else if(color.equalsIgnoreCase("green")) {
+			rob.setPenColor(Color.green);
+		}else if(color.equalsIgnoreCase("blue")) {
+			rob.setPenColor(Color.blue);
+		}else {
+			rob.setRandomPenColor();
+		}
+		
+		rob.penDown();
+		rob.setSpeed(10);
+		for(int i=0; i<3; i++) {
+			rob.turn(120);
+			rob.move(100);
+		}
+		
+		
+	}
+	
+	static void drawCircle() {
+
+		if(color.equalsIgnoreCase("red")) {
+			rob.setPenColor(Color.red);
+		}else if(color.equalsIgnoreCase("green")) {
+			rob.setPenColor(Color.green);
+		}else if(color.equalsIgnoreCase("blue")) {
+			rob.setPenColor(Color.blue);
+		}else {
+			rob.setRandomPenColor();
+		}
+		
+		rob.penDown();
+		rob.setSpeed(100);
+		for(int i=0; i<360; i++) {
+			rob.turn(1);
+			rob.move(1);
+		}
+		
+	}
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -6,25 +6,37 @@ public class HappyPet {
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
 	
+	static int happinessLevel = 0;
+	
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String typeOfPet = JOptionPane.showInputDialog("What kind of pet do you want to buy? A dog or a cat.");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
-		
+		for(int i=0; i<100; i++) {
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "You want to make your pet happy, what shall you do?", "Pet Option", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Feed it", "Take it on a walk", "Pet it" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
-
+			
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+		}
 	}
 
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	
+	static void feed(String pet) {
+		if(pet.equalsIgnoreCase("cat")) {
+			JOptionPane.showMessageDialog(null, "You open a can of cat food and poor it in a bowl, your cat walks over and takes its time to enjoy the food. It purrs and rubs on you when it is done.");
+		}else if(pet.equalsIgnoreCase("dog")) {
+			
+		}
+		
+	}
+	
 }
