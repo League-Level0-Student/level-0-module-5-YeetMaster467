f1d5a5dd4508faa437749c2812aae449246f288c
YeetMaster467 <elicomixinc@gmail.com>
2022-02-02 19:57:53 -0800

    Completed Code4Life

--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -15,18 +15,28 @@ public class Code4Life {
 	 * *****************/
 	static boolean canPlaySounds = true;
 	
-	
-	// 1. Ask the user how many hours they spent coding this week.
+	public static void main(String[] args) {
+		// 1. Ask the user how many hours they spent coding this week.
+		String input = JOptionPane.showInputDialog("How many hours did you spend coding this week?");
+		int hours = Integer.parseInt(input);
+		// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
+		// write code instead.
+		if(hours <= 2) {
+			JOptionPane.showMessageDialog(null, "Stop watching YouTube and go write some code.");
+		}else if(hours >= 3 && hours <= 5) {
+			JOptionPane.showMessageDialog(null, "You're a code ninja!");
+		}else if(hours > 5) {
+			playBatmanTheme();
+			displayBatman();
+		}
+		// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
+		// they're a Code Ninja
 
-	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
-	// write code instead.
+		// 4. If it is more than 5, call the playBatmanTheme  method, then 
+		//    call the displayBatman method.
 
-	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
-	// they're a Code Ninja
-
-	// 4. If it is more than 5, call the playBatmanTheme  method, then 
-	//    call the displayBatman method.
-
+		
+	}
 
 
 	
